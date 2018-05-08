7d9e280d3123320164453f57d114e92dcaf496bf
14 <14>
2018-05-08 11:54:27 +0300

    (*) robots

---
+++ Robots/20/20.cpp
@@ -0,0 +1,15 @@
+// 20.cpp: определяет процедуры инициализации для библиотеки DLL.
+//
+
+#include "stdafx.h"
+#include "20.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+using namespace std;
+
+void DoStep(Stepinfo* si)
+{
+
+}
---
+++ Robots/21/21.cpp
@@ -0,0 +1,17 @@
+// 21.cpp: определяет процедуры инициализации для библиотеки DLL.
+//
+
+#include "stdafx.h"
+#include "21.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+using namespace std;
+
+void DoStep(Stepinfo* si)
+{
+
+}
+
+
